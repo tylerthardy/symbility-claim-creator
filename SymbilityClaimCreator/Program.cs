861b75a7c2da10eab267047cc33b2f603284c862
Tyler Hardy <tylerthardy@gmail.com>
2021-05-18 20:21:18 -0500

    Scaffold configuration

--- SymbilityClaimCreator/Program.cs
+++ SymbilityClaimCreator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace SymbilityClaimCreator
 {
@@ -6,7 +8,40 @@ namespace SymbilityClaimCreator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var host = CreateHostBuilder(args).Build();
+
+            Console.ReadKey();
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(config =>
+                {
+                    if (args != null)
+                    {
+                        // arguments passed from command line
+                        // e.g.: dotnet run --environment "Staging"
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    var env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+                    var configurationRoot = configuration.Build();
+
+                    var claimSourceConfiguration = new SymbilityApiConfiguration();
+                    var assigneeConfiguration = new SymbilityApiConfiguration();
+                    var secondAssigneeConfiguration = new SymbilityApiConfiguration();
+                    configurationRoot.GetSection("ClaimSourceConfiguration").Bind(claimSourceConfiguration);
+                    configurationRoot.GetSection("AssigneeConfiguration").Bind(assigneeConfiguration);
+                    configurationRoot.GetSection("SecondAssigneeConfiguration").Bind(secondAssigneeConfiguration);
+                });
     }
 }
---
+++ SymbilityClaimCreator/SymbilityApiConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SymbilityClaimCreator
+{
+    public class SymbilityApiConfiguration
+    {
+        public string ApiUrl { get; set; }
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+    }
+}
